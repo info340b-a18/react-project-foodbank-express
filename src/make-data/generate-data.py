69a2a4ab93ebcbd404bbc2153f737c83f381f2ec
aarya22 <aman.arya524@gmail.com>
2018-11-27 11:45:10 -0800

    Add all foodBanks

--- src/make-data/generate-data.py
+++ src/make-data/generate-data.py
@@ -3,9 +3,10 @@ import random as r
 import numpy as np
 
 imperishable = ["Cheese", "Dry Milk", "Canned Tuna", "Canned Salmon", "Nuts", "Nut Butter", "Sardines", "Black Beans", "Crackers", "Beef jerky", "Applesauce", "Pudding", "Fruit Cups", "Granola Bars", "Power Bars", "Cereal Bars", "Canned Beans", "Ravioli", "Spaghetti Cans", "Canned Soup", "Ramen", "Tomato Juice", "Packaged Nuts", "Trail Mix", "Fruit Snacks", "Dried Fruits", "Peanut Butter", "Pasta", "Spaghetti Sauce", "Canned Vegetables", "Fruit", "Macaroni", "Chips", "Pretzels", "Goldfish", "Boxed Drinks", "Juices", "Vienna Sausage", "Pop Tarts", "Oatmeal", "Ziplock", "Canned Tuna", "Rice", "Beans", "Dry Milk", "Cereal", "Protein Bars", "Raisins", "Prunes", "Peas", "Barley", "Quinoa", "Pudding"]
-perishable = ["Fish", "Chicken", "Beef", "Pork", "Mutton", "sausage", "Milk", "Apples", "Vegetables", "Carrots", "Green Beans", "artichokes", "legumes", "broccoli", "celery", "bok choy", "poultry", "crab", "meatballs"]
-food_banks = ["Rainier Valley Food Bank", "YWCA Central Area Food Bank", "Blessed Sacrament", "North Helpline", "Phinney Ridge Food Bank", "Greenwood Food Bank", "Ballard Food Bank", "Paradise of Praise", "West Seattle Food Bank", "El Centro de la Raza"]
 
+perishable = ["Fish", "Chicken", "Beef", "Pork", "Mutton", "sausage", "Milk", "Apples", "Vegetables", "Carrots", "Green Beans", "artichokes", "legumes", "broccoli", "celery", "bok choy", "poultry", "crab", "meatballs"]
+
+food_banks = ["Asian Counseling and Referral Services Food Bank", "Ballard Food Bank", "Blessed Sacrament Food Bank", "Byrd Barr Place Food Bank", "Cherry Street Food Bank", "Chicken Soup Brigade", "El Centro de la Raza", "FamilyWorks - Wallingford Food Bank", "FamilyWorks - Greenwood Food Bank", "Giving Room Food Bank @ Epic Life Church", "Immanuel Community Services", "Jewish Family Service", "North Helpline - Lake City Food Bank", "North Helpline - Bitter Lake Food Bank", "Paradise of Praise", "Phinney Ridge Food Bank", "Pike Market Food Bank", "Providence Regina House", "Puget Sound Labor Agency", "Queen Anne Food Bank at Sacred Heart", "Rainier Valley Food Bank", "Salvation Army Food Bank", "Seattle Indian Center", "St. Vincent de Paul Food Bank", "The Food Bank @ St. Mary's", "University District Food Bank", "West Seattle Food Bank", "White Center Food Bank", "YWCA Central Area Food Bank"]
 
 def generateRandomFood():
     r.shuffle(imperishable)
@@ -15,7 +16,6 @@ def generateRandomFood():
     return dict(zip(foods, weights))
     
 generateRandomFood()
-
 
 def generateFoodBankJson():
     bank_words = {}
